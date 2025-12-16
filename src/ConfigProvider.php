e14fdb7b92a7ecd96253c75cd888ae1aad2bb802
George Steel <george@net-glue.co.uk>
2025-12-16 09:30:57 +0000

    Initial repository scaffolding and commit
    Signed-off-by: George Steel <george@net-glue.co.uk>

---
+++ src/ConfigProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Laminas\I18n\Validator;
+
+final readonly class ConfigProvider
+{
+    /** @return array<string, mixed> */
+    public function __invoke(): array
+    {
+        return [];
+    }
+}
